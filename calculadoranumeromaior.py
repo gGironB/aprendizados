5ec6611b64b7e22462fc7474d7dcfa53cc98b472
Gustav <89752376+gGironB@users.noreply.github.com>
2026-03-17 23:45:24 -0300

    Calculadora que mostra o maior dos três números.

---
+++ calculadoranumeromaior.py
@@ -0,0 +1,13 @@
+
+
+n1 = float(input("DIGITE O PRIMEIRO NUMERO: "))
+n2 = float(input("DIGITE O SEGUNDO NUMERO: "))
+n3 = float(input("DIGITE O TERCEIRO NUMERO: "))
+def conta_media (n1,n2,n3):
+    resultado = n1 + n2 + n3
+    resultado2 = resultado / 3
+    return resultado2
+x = conta_media (n1,n2,n3)
+print(x)
+
+
