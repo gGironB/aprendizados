8fd4c65c11f8c59067f54ba71ffba0e371bc3acd
Gustav <89752376+gGironB@users.noreply.github.com>
2026-04-13 07:57:12 -0300

    segunda vers'

---
+++ appacademia.py
@@ -0,0 +1,23 @@
+treinos = []
+while True:
+    print("1 - Adicionar treino")
+    print("2 - Ver treinos")
+    print("3 - Sair")
+    
+    opcao = input("Escolha : ")
+    if opcao == "1":
+        exercicio = input("Escolha um exercicio: ")
+        reps =  int(input("Quantas reps: "))
+        peso = int(input("Peso: "))
+        treinos.append ([exercicio,reps,peso])
+        print("Treino salvo!")
+        
+    elif opcao == "2":
+        if not treinos:
+            print("Nenhum treino salvo!")
+        else:
+            for t in treinos:
+                print(t)
+            
+    elif opcao == "3":
+        break
