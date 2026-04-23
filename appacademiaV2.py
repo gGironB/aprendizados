e1a8400682be45b56fed3d31015d87310a6a5351
Gustav <89752376+gGironB@users.noreply.github.com>
2026-04-22 23:24:32 -0300

    atualização 3

--- appacademiaV2.py
+++ appacademiaV2.py
@@ -24,10 +24,25 @@ def mostrar_treino(treinos):
                for i, t in enumerate(treinos):
                     print(f"{i+1} - Exercicio: {t[0]} / Reps: {t[1]} / Peso: {t[2]}")
 
+
+def remover_treino(treinos):
+     if not treinos:
+          print("Nenhum treino adicionado!")
+          return # "return serve para sair cedo da funcao, no caso desse, visto que nao tem treino para remover, apenas informa o mesmo"
+     mostrar_treino(treinos)
+     numero = int(input("Qual treino remover?"))
+     indice = numero - 1
+     if indice >= 0 and indice < len(treinos):
+          treinos.pop(indice)     
+          print("Treino removido! ")
+     else:
+          print("Treino invalido! ")
+
 while True:
     print("1 - Adicionar treino")
     print("2 - Ver treinos")
-    print("3 - Sair")
+    print("3 - Remover treino")
+    print("4 - Sair")
     opcao = input("Escolha : ")
 
     if opcao == "1":
@@ -35,7 +50,9 @@ while True:
     elif opcao == "2":
          mostrar_treino(treinos)
     elif opcao == "3":
-         break
+          remover_treino(treinos)
+    elif opcao == "4":
+     break
     else:
          print("Opção invalida")
     
